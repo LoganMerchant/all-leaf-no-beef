53590c55b9f662ac63a46c26eea1002a4d49f67b
Logan Merchant <merchantclogan@gmail.com>
2020-11-05 17:35:51 -0700

    add initial model ddefinitions for user, post, comment

---
+++ models/User.js
@@ -0,0 +1,48 @@
+// Imports
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+
+class User extends Model {};
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: true
+            }
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                len: [5, 25]
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4, 20]
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        modelName: 'user',
+        underscored: true     
+    }
+);
+
+module.exports = User;
