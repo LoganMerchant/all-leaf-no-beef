5edc547bb2845ef913b10d3549665a8370a0c5cf
Logan Merchant <merchantclogan@gmail.com>
2020-11-07 22:00:29 -0700

    add example function of sms timer

--- send-sms.js
+++ send-sms.js
@@ -4,10 +4,26 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_ACCOUNT_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
-client.messages
-    .create({
-        body: 'Reminder: Water your tomatoes today!',
-        from: '+12058966426',
-        to: '+18015542378'
-    })
-    .then(message => console.log(message.sid));
+// BASIC OUTBOUND SMS 
+// client.messages
+//     .create({
+//         body: 'Reminder: Water your tomatoes today!',
+//         from: '+12058966426',
+//         to: '+18015542378'
+//     })
+//     .then(message => console.log(message.sid));
+
+// TEST OUTBOUND ON TIMER (EVERY MINUTE)
+// const CronJob = require('cron').CronJob;
+
+// const job = new CronJob('*/1 * * * *', function() {
+//     client.messages
+//         .create({
+//             body: `${new Date().getHours()} / ${new Date().getMinutes()} / ${new Date().getSeconds()}`,
+//             from: '+12058966426',
+//             to: '+18015542378'
+//         })
+//         .then(message => console.log(message.sid));
+// });
+
+// job.start();
