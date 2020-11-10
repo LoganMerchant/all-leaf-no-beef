1b02fb5f035c9c7342fb29423e5cab45bc4504dd
Chelsea Thurman <babefan200@gmail.com>
2020-11-10 14:20:09 -0700

    added forum

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const session = require('express-session');
 
 // Imports from elsewhere in this directory
 const sequelize = require('./config/connection');
+const login = require("./routes/loginroutes");
+const topic = require("./routes/topicRoutes");
+const comment = require("./routes/commentRoutes");
 
 // SESSION SETUP
     // This sets Sequelize up to have sessions
@@ -54,3 +57,59 @@ sequelize.sync({ force: false })
     .then( () => {
         app.listen(PORT, () => console.log(`Now listening on PORT ${PORT}`));
 });
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+  
+  app.get("/", (req, res) => {
+    res.json({ status: "hello" });
+  });
+  app.post("/register", login.register);
+  app.post("/login", login.login);
+  app.post("/reset", login.reset);
+  
+  app.post("/createtopic", verifyToken, topic.createTopic);
+  app.put("/modifytopic", verifyToken, topic.modifyTopic);
+  app.get("/findtopic", verifyToken, topic.getTopic);
+  app.delete("/deletetopic", verifyToken, topic.deleteTopic);
+  app.get("/getall", verifyToken, topic.getAll);
+  
+  app.post("/createcomment", verifyToken, comment.createComment);
+  app.put("/modifycomment", verifyToken, comment.modifyComment);
+  app.get("/findcomment", verifyToken, comment.getComment);
+  app.delete("/deletecomment", verifyToken, comment.deleteComment);
+  app.get("/search-comments", verifyToken, comment.searchText);
+  
+  function verifyToken(req, res, next) {
+    const Header = req.headers["authorization"];
+    if (typeof Header !== "undefined") {
+      req.token = Header;
+      jwt.verify(req.token, "secretkey", (err, authData) => {
+        if (err) {
+          console.log(err);
+          res.sendStatus(403);
+        } else {
+          req.email = authData.users.email;
+          next();
+  
+          // res.json({
+          //   message: 'Post created...',
+          //   authData
+  
+          // });
+        }
+      });
+    } else {
+      res.sendStatus(403);
+    }
+  }
+  
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
