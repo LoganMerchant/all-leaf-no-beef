b4b9bb944e424db39ff04aeb3128b3b51db1f542
christian.dehek <christian.dehek@gmail.com>
2020-11-11 13:41:37 -0700

    starting working on signup.handlebars with the javascript

---
+++ public/javascript/signup.js
@@ -0,0 +1,30 @@
+async function signupFormHandler(event) {
+    event.preventDefault();
+  
+    const username = document.querySelector('#username').value.trim();
+    const email = document.querySelector('#email').value.trim();
+    const password = document.querySelector('#password').value.trim();
+    const phone_number = document.querySelector('#phone_number').value.trim();
+  
+    if (username && email && phone_number && password) {
+      const response = await fetch('/api/users', {
+        method: 'post',
+        body: JSON.stringify({
+          username,
+          email,
+          phone_number,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      // check the response status
+      if (response.ok) {
+        console.log('success');
+      } else {
+        alert(response.statusText);
+    }
+  }
+}
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler)
