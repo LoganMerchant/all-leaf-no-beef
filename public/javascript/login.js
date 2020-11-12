3a3bb9ed6518ccea51bb017337f0683e2fd3b3f0
christian.dehek <christian.dehek@gmail.com>
2020-11-11 18:44:06 -0700

    mostly finished with logic for login and signup forms

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,8 +1,8 @@
 async function loginFormHandler(event) {
     event.preventDefault();
   
-    const email = document.querySelector('#email').value.trim();
-    const password = document.querySelector('#password').value.trim();
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
   
     if (email && password) {
       const response = await fetch('/api/users/login', {
@@ -15,6 +15,7 @@ async function loginFormHandler(event) {
       });
   
       if (response.ok) {
+        console.log('success');
         document.location.replace('/');
       } else {
         alert(response.statusText);
