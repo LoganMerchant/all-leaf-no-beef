6266f66b32ed663e2c77ce1068417370ee472134
christian.dehek <christian.dehek@gmail.com>
2020-11-12 14:24:43 -0700

    working on add-tracked-produce.js

--- public/javascript/add-tracked-produce.js
+++ public/javascript/add-tracked-produce.js
@@ -1,5 +1,10 @@
 async function addProducetoUserHandler(event) {
     event.preventDefault();
 
+    const produceName = document.getElementById("produce-name").value.trim();
+
+    console.log(produceName);
     
 }
+
+document.querySelector('#add-produce').addEventListener('click', addProducetoUserHandler);
