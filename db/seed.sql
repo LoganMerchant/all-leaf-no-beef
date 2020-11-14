109fd92e1a1b9e2bfc87fbaccf6a6c9d8749dd89
Joshua Gerner <joshuagerner@Joshuas-MacBook-Pro-4.local>
2020-11-13 21:49:56 -0700

    CSS color changes, beginings of border controls, button changes

--- db/seed.sql
+++ db/seed.sql
@@ -2,7 +2,7 @@ USE all_leaf_no_beef;
 
 INSERT INTO produce ( id, name, water_amount, water_frequency, planting_depth, pot_size, amount_of_light) 
 VALUES 
-    (DEFAULT, "carrots", "1 inch", "daily", "1 1/2 inches", 12, 6),
+    (DEFAULT, "carrots", "1 inch", "daily", "1 1/2 inches", 12, 6,),
     (DEFAULT, "radishes", "2 inches", "daily", "1/2 inch", 6, 5),
     (DEFAULT, "lettuce", "4 inches", "bi-weekly", "1/2 inch", 3, 6),
     (DEFAULT, "tomatoes", "2 inches", "daily", "12 inches", 18, 8),
@@ -11,4 +11,4 @@ VALUES
     (DEFAULT, "broccoli", "1 inch", "daily", "1/2 inch", 12, 6),
     (DEFAULT, "scallions", "1 inch", "daily", "1/2 inch", 6, 8),
     (DEFAULT, "onions", "1/2 inch", "daily", "1 1/2 inches", 8, 15),
-    (DEFAULT, "spinach", "1/2 inch",  "bi-weely", "1 inch", 8, 4);
+    (DEFAULT, "spinach", "1/2 inch",  "bi-weekly", "1 inch", 8, 4);
