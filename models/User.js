be4a158c55822705f3a342d621e52d63f687f549
Logan Merchant <merchantclogan@gmail.com>
2021-03-12 20:43:34 -0700

    made adding phone number optional

--- models/User.js
+++ models/User.js
@@ -1,67 +1,71 @@
 // Imports
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
-const bcrypt = require('bcrypt');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+const bcrypt = require("bcrypt");
 
 class User extends Model {
-    // setup method to run on instance data (per user) to check password
-    checkPassword(loginPw) {
-        return bcrypt.compareSync(loginPw, this.password);
-    }
-};
+  // setup method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 User.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-            validate: {
-                isEmail: true
-            }
-        },
-        phone_number: {
-            type: DataTypes.STRING(10),
-            allowNull: false,
-            validate: {
-                isAlphanumeric: true
-            }
-        },
-        username: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [4, 20]
-            }
-        }
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        hooks: {
-            async beforeCreate(newUserData) {
-                newUserData.password = await bcrypt.hash(newUserData.password, 10);
-                return newUserData;
-            },
-            async beforeUpdate(updatedUserData) {
-                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-                return updatedUserData;
-            }
-        },
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        modelName: 'user',
-        underscored: true     
-    }
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    phone_number: {
+      type: DataTypes.STRING(10),
+      allowNull: true,
+      validate: {
+        isAlphanumeric: true,
+      },
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4, 20],
+      },
+    },
+  },
+  {
+    hooks: {
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
+        return updatedUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    modelName: "user",
+    underscored: true,
+    omitNull: true,
+  }
 );
 
 module.exports = User;
--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,31 +1,34 @@
 async function signupFormHandler(event) {
-    event.preventDefault();
-  
-    const username = document.querySelector('#username').value.trim();
-    const email = document.querySelector('#email').value.trim();
-    const password = document.querySelector('#password').value.trim();
-    const phone_number = document.querySelector('#phone_number').value.trim();
-  
-    if (username && email && phone_number && password) {
-      const response = await fetch('/api/users', {
-        method: 'post',
-        body: JSON.stringify({
-          username,
-          email,
-          phone_number,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-  
-      // check the response status
-      if (response.ok) {
-        console.log('success');
-        document.location.replace('/');
-      } else {
-        alert(response.statusText);
+  event.preventDefault();
+
+  const username = document.querySelector("#username").value.trim();
+  const email = document.querySelector("#email").value.trim();
+  const password = document.querySelector("#password").value.trim();
+  const phone_number =
+    document.querySelector("#phone_number").value.trim() || null;
+
+  if (username && email && password) {
+    const response = await fetch("/api/users", {
+      method: "post",
+      body: JSON.stringify({
+        username,
+        email,
+        phone_number,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    // check the response status
+    if (response.ok) {
+      console.log("success");
+      document.location.replace("/");
+    } else {
+      alert(response.statusText);
     }
   }
 }
 
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler)
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
